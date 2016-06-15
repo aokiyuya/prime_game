c7671ee4971fc053917d4842aa3d27abb12fc5bb
aokiyuya <yuya.aoki@fujielab.org>
2016-06-15 11:42:49 +0900

    third_primeがMacBook Air(i7 2.2GHz, 8GB)で0.061秒です。たぶんこれがいちばん早いと思います

---
+++ third_prime.cpp
@@ -0,0 +1,26 @@
+// Author: yuya aoki
+// solve prime number using Sieve of Eratosthenes
+#define MAXNUM 100000
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char *argv[]){
+	int list[MAXNUM];
+	int first_num;
+	for(int i = 0 ; i <= MAXNUM - 2 ; i++){
+		list[i] = i + 2;
+	}
+	for(int tmp = 0 ; tmp <= sqrt(MAXNUM) ; tmp++){
+		if (list[tmp] == 0) continue;
+		first_num = list[tmp];
+		for(int temp = tmp + 1 ; temp <= MAXNUM ; temp++){
+			if(list[temp]%first_num == 0) list[temp] = 0;
+		}
+	}
+	for(int tmp = 0 ; tmp <= MAXNUM ; tmp ++){
+		if (list[tmp] == 0) continue;
+		printf("%d\n", list[tmp]);
+		}
+	return 0;
+}
+
